96dcf0305af172de05db716698823ab37b7390e7
Nithin345256 <nithinmalve009@gmail.com>
2025-09-28 15:42:28 +0530

    new commit

--- backend/routes/filteroutes.js
+++
@@ -1,8 +0,0 @@
-import { getHotelByType } from "../controllers/getHotelByType.js";
-import express from "express";
-const router = express.Router();
-
-// Get hotels by type
-router.get("/", getHotelByType);
-
-export default router;
